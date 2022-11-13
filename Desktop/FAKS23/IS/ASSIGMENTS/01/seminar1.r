052b3816489fea17b5f1213810f02e2687de180e
Roki2k <117859046+Roki2k@users.noreply.github.com>
2022-11-13 10:40:28 +0100

    Update seminar1.r
    Commit pred dodajanjem labirinta v vsakega agenta (Also - dodan izpis korakov na koncu)

--- Desktop/FAKS23/IS/ASSIGMENTS/01/seminar1.r
+++ Desktop/FAKS23/IS/ASSIGMENTS/01/seminar1.r
@@ -8,17 +8,20 @@ source("mazes.r")
 #         "#S..#",
 #         "#####")#maze1
 
-m1 <- maze2
+{
+m1 <- maze3
 
 print(m1)
 
 m1 <- str_split(m1, "", Inf, simplify = TRUE)
 m1
-start <- which(m1 == "S", arr.ind = T)
-finish <- which(m1 == "E", arr.ind = T)
+start <- which(m1 == "S", arr.ind = TRUE)
+finish <- which(m1 == "E", arr.ind = TRUE)
+}
 
 
-fitnes <- function(v_ukazi) {
+fitnes <- function(v_ukazi, maze) {
+  maze
   score <- 0
   #v_ukazi = str_split(ukazi, "", simplify = T)
   pos <- start
@@ -41,39 +44,39 @@ fitnes <- function(v_ukazi) {
     #        pos <- pos
     #)
     
-    if(v_ukazi[i] >= 0 & v_ukazi[i] < 1){ # korak v levo
+    if(v_ukazi[i] >= 0 && v_ukazi[i] < 1){ # korak v levo
       if(m1[pos[1], pos[2] - 1] != "#"){
         pos[2] <- pos[2] - 1
       }
       else{
-        score <- score + 2
+        score <- score + 5
       }
     }
     
-    if(v_ukazi[i] >= 1 & v_ukazi[i] < 2){ # korak v desno
+    if(v_ukazi[i] >= 1 && v_ukazi[i] < 2){ # korak v desno
       if(m1[pos[1], pos[2] + 1] != "#"){
         pos[2] <- pos[2] + 1
       }
       else{
-        score <- score + 2
+        score <- score + 5
       }
     }
     
-    if(v_ukazi[i] >= 2 & v_ukazi[i] < 3){ # korak gor
+    if(v_ukazi[i] >= 2 && v_ukazi[i] < 3){ # korak gor
       if(m1[pos[1] - 1, pos[2]] != "#"){
         pos[1] <- pos[1] - 1
       }
       else{
-        score <- score + 2
+        score <- score + 5
       }
     }
     
-    if(v_ukazi[i] >= 3 & v_ukazi[i] < 4){ # korak dol
+    if(v_ukazi[i] >= 3 && v_ukazi[i] < 4){ # korak dol
       if(m1[pos[1] + 1, pos[2]] != "#"){
         pos[1] <- pos[1] + 1
       }
       else{
-        score <- score + 2
+        score <- score + 5
       }
     }
     
@@ -88,19 +91,18 @@ fitnes <- function(v_ukazi) {
     #}
     if ( i == 1){ # kaznujemo backtrackanje, da spodbudimo premikanje
       score = score
-    } else if ((v_ukazi[i] >= 0 & v_ukazi[i] < 1) & (v_ukazi[i-1] >= 1 & v_ukazi[i-1] < 2)) {
-      score = score + 1
-    } else if ((v_ukazi[i] >= 1 & v_ukazi[i] < 2) & (v_ukazi[i-1] >= 0 & v_ukazi[i-1] < 1)) {
-      score = score + 1
-    } else if ((v_ukazi[i] >= 2 & v_ukazi[i] < 3) & (v_ukazi[i-1] >= 3 & v_ukazi[i-1] < 4)) {
-      score = score + 1
-    } else if ((v_ukazi[i] >= 3 & v_ukazi[i] < 4) & (v_ukazi[i-1] >= 2 & v_ukazi[i-1] < 3)) {
-      score = score + 1
+    } else if ((v_ukazi[i] >= 0 && v_ukazi[i] < 1) && (v_ukazi[i-1] >= 1 && v_ukazi[i-1] < 2)) {
+      score = score + 4
+    } else if ((v_ukazi[i] >= 1 && v_ukazi[i] < 2) && (v_ukazi[i-1] >= 0 && v_ukazi[i-1] < 1)) {
+      score = score + 4
+    } else if ((v_ukazi[i] >= 2 && v_ukazi[i] < 3) && (v_ukazi[i-1] >= 3 && v_ukazi[i-1] < 4)) {
+      score = score + 4
+    } else if ((v_ukazi[i] >= 3 && v_ukazi[i] < 4) && (v_ukazi[i-1] >= 2 && v_ukazi[i-1] < 3)) {
+      score = score + 4
     }
 
     if(m1[pos[1],pos[2]] == "E"){
       return(-score)
-      end = true
     }
 
     score <- score + 1
@@ -115,11 +117,40 @@ fitnes <- function(v_ukazi) {
   
 }
 
+{
 nSteps <- (length(m1[1,])-2)*(length(m1[,1])-2)
 print(nSteps)
 lBound <- rep(0, nSteps)
 uBound <- rep(4, nSteps)
+}
 
-GA <- ga(type = "real-valued", fitness = fitnes, lower = lBound, upper = uBound)
+GA <- ga(type = "real-valued", fitness = fitnes, lower = lBound, upper = uBound, maxiter = 300)
 plot(GA)
-print(floor(GA@solution[1,]))
+
+resitev = floor(GA@solution[1,])
+
+for (i in 1: length(resitev)) {
+    #switch( v_ukazi[i],
+    #        pos[2] <- pos[2] - 1,
+    #        pos[2] <- pos[2] + 1,
+    #        pos[1] <- pos[1] - 1,
+    #        pos[1] <- pos[1] + 1,
+    #        pos <- pos
+    #)
+    
+    if(resitev[i] >= 0 && resitev[i] < 1){ # korak v levo
+      print("L ")
+    }
+    
+    if(resitev[i] >= 1 && resitev[i] < 2){ # korak v desno
+       print("R ")
+    }
+    
+    if(resitev[i] >= 2 && resitev[i] < 3){ # korak gor
+      print("U ")
+    }
+    
+    if(resitev[i] >= 3 && resitev[i] < 4){ # korak dol
+      print("D ")
+    }
+}
